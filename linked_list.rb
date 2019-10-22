32d6fd1145c1424d5a96a44bf2458c0198ae039e
Liz Denhup <lizdenhup@gmail.com>
2019-10-22 15:33:30 -0500

    initial commit

---
+++ linked_list.rb
@@ -0,0 +1,16 @@
+class Node
+  attr_accessor :val, :next
+
+  def initialize(val:, next: nil)
+    @val = val
+    @next = next
+  end
+end
+
+class SinglyLinkedList
+
+end
+
+class DoublyLinkedList
+
+end
