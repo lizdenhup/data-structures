2ab99cf1343eda64e64784265258e40f5cf992e4
Liz Denhup <lizdenhup@gmail.com>
2020-03-13 11:48:52 -0700

    remove dupes from array in place

---
+++ two-pointers.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+class RemoveDuplicates
+  attr_reader :nums
+
+  def initialize(nums)
+    @nums = nums
+  end
+
+  def remove_duplicates
+    i = 0
+    j = i + 1
+
+    while i <= nums.length - 1
+      j = i + 1
+      while j <= nums.length - 1
+        if nums[j] == nums[i]
+          nums.delete_at(j)
+        else
+          j += 1
+        end
+      end
+      i += 1
+    end
+    puts "#{nums}"
+    nums
+  end
+
+end
+
+
+RemoveDuplicates.new([0,0,1,1,1,2,2,3,3,4]).remove_duplicates
