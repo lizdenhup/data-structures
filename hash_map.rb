af592e3cc5386180d3fe59d0203296130b280c81
Liz Denhup <lizdenhup@gmail.com>
2020-03-18 16:49:55 -0700

    implement hash map

---
+++ hash_map.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+
+# Implement the hash map interface yourself.
+# Of course, the interface should support the following
+# O(1) operations: search, insert, delete.
+# Use separate chaining to deal with collisions.
+# In this simple example keys will be single letters or integers
+
+class HashMap
+  ALPHABET_MAP = {"a"=>0, "b"=>1, "c"=>2, "d"=>3, "e"=>4, "f"=>5, "g"=>6, "h"=>7, "i"=>8, "j"=>9, "k"=>10, "l"=>11, "m"=>12, "n"=>13, "o"=>14, "p"=>15, "q"=>16, "r"=>17, "s"=>18, "t"=>19, "u"=>20, "v"=>21, "w"=>22, "x"=>23, "y"=>24, "z"=>25}
+
+  def initialize
+  end
+
+  def self.hash_function(input)
+    if input.class == "String"
+      ALPHABET_MAP[str] % 7
+    else
+      input % 7
+    end
+  end
+
+  def put(key, val)
+  end
+
+  def remove(key)
+  end
+
+  def get(key)
+  end
+
+  def get_random
+  end
+
+end
+
+# hm = HashMap.new
+# puts "#{hm.put('a', 32)}"
+puts "#{HashMap.hash_function(33)}"
